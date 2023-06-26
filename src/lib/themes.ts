9eba6d2b07d1d72415a5065c484c68f27734c337
Callan Barrett <callan@zoocar.org>
2023-06-26 20:40:36 +0800

    Add mister consoles theme

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -1,59 +1,80 @@
 import { ThemeOptions } from "@mui/material";
 
 interface ThemeDefinition {
-    id: string;
-    displayName: string;
-    options: ThemeOptions;
+  id: string;
+  displayName: string;
+  options: ThemeOptions;
 }
 
 export const themes: { [id: string]: ThemeDefinition } = {
-    tron: {
-        id: "tron",
-        displayName: "Tron",
-        options: {
-            palette: {
-                mode: "dark",
-                primary: {
-                    main: "#13e8ec",
-                },
-                secondary: {
-                    main: "#13e8ec",
-                },
-                text: {
-                    primary: "#13e8ec",
-                    secondary: "#0a6b6d",
-                }
-            },
+  tron: {
+    id: "tron",
+    displayName: "Tron",
+    options: {
+      palette: {
+        mode: "dark",
+        primary: {
+          main: "#13e8ec",
         },
+        secondary: {
+          main: "#13e8ec",
+        },
+        text: {
+          primary: "#13e8ec",
+          secondary: "#0a6b6d",
+        },
+      },
     },
-    mister: {
-        id: "mister",
-        displayName: "MiSTer",
-        options: {
-            palette: {
-                mode: "dark",
-                background: {
-                    default: "#282a36",
-                },
-                primary: {
-                    main: "#9c5cf7",
-                    contrastText: "#fff",
-                },
-                secondary: {
-                    main: "#514689",
-                },
-                text: {
-                    primary: "#fff",
-                }
-            },
+  },
+  mister: {
+    id: "mister",
+    displayName: "MiSTer",
+    options: {
+      palette: {
+        mode: "dark",
+        background: {
+          default: "#282a36",
         },
-    }
+        primary: {
+          main: "#9c5cf7",
+          contrastText: "#fff",
+        },
+        secondary: {
+          main: "#514689",
+        },
+        text: {
+          primary: "#fff",
+        },
+      },
+    },
+  },
+  misterConsoles: {
+    id: "misterConsoles",
+    displayName: "MiSTer Consoles",
+    options: {
+      palette: {
+        mode: "dark",
+        primary: {
+          main: "#257cc7",
+        },
+        secondary: {
+          main: "#d31e2b",
+        },
+        warning: {
+          main: "#efa400",
+        },
+        success: {
+          main: "#2bc32b",
+        },
+      },
+    },
+  },
 };
 
 export function getTheme(id: string): ThemeDefinition {
-    if (themes[id]) {
-        return themes[id];
-    } else {
-        return themes.mister;
-    }
+  if (themes[id]) {
+    return themes[id];
+  } else {
+    return themes.mister;
+  }
 }
